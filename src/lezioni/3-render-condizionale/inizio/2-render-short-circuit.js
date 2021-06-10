8f9f3e1f5f9f9f1af3080491d471cde0daf36f66
Guy Roger Eboulet <eboulet87@gmail.com>
2021-06-10 09:13:47 +0200

    Short circuit evaluation

--- src/lezioni/3-render-condizionale/inizio/2-render-short-circuit.js
+++ src/lezioni/3-render-condizionale/inizio/2-render-short-circuit.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 
 const ShortRender = () => {
   const [parola, setParola] = useState("Sono una parola");
-  const esempio = parola || ""
+
+  // Short circuit Evaluation with an (||) operartor
+  const esempio = parola || "sono un esempio";
+  // Short circuit evaluation with an (&&) operator
+  const esempio2 = parola && "sono un esempio";
   return (
     <div>
-      <h2>short render Component</h2>
+      <h2>{esempio}</h2>
+      {parola && <h4>Ciao sono un componente</h4>}
     </div>
   );
 };
