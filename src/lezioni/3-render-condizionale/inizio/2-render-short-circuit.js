a02c77a269da71d7bc481776c5926abb9857e82d
Guy Roger Eboulet <eboulet87@gmail.com>
2021-06-10 15:04:34 +0200

    Ternary operator

--- src/lezioni/3-render-condizionale/inizio/2-render-short-circuit.js
+++ src/lezioni/3-render-condizionale/inizio/2-render-short-circuit.js
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 
 const ShortRender = () => {
   const [parola, setParola] = useState("Sono una parola");
+  const [toggle, setToggle] = useState(true);
 
-  // Short circuit Evaluation with an (||) operartor
-  const esempio = parola || "sono un esempio";
-  // Short circuit evaluation with an (&&) operator
-  const esempio2 = parola && "sono un esempio";
+  // // Short circuit Evaluation with an (||) operartor
+  // const esempio = parola || "sono un esempio";
+  // // Short circuit evaluation with an (&&) operator
+  // const esempio2 = parola && "sono un esempio";
+  // // Ternary operator esempio
+  // const esempio3 = parola === "Valore" ? "Sono Vero" : "Sono Falso";
   return (
     <div>
-      <h2>{esempio}</h2>
-      {parola && <h4>Ciao sono un componente</h4>}
+      <h2>{parola || "Sono un esempio se non è inserito un valore"}</h2>
+      {toggle ? (
+        <h2 className="text-success"> Vero </h2>
+      ) : (
+        <h5 className="text-success"> Falso </h5>
+      )}
+      <button className="button" onClick={() => setToggle(!toggle)}>
+        Cambia
+      </button>
     </div>
   );
 };
